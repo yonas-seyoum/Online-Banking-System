9cbc27577755a8657868420ccadb32e15ceda1ad
Yonas <yonasseyoum24@gmail.com>
2024-02-10 12:20:40 +0300

    add transfer window component

---
+++ app/src/pages/transfer/component/transferWindow.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function TransferWindow() {
+  return (
+    <div className="transfer-window">
+      <h3>Transfer to YoBank Account</h3>
+      <div className="item-holder">
+        <label>Account No</label>
+        <input type="number" />
+      </div>
+      <div className="item-holder">
+        <label>From Account</label>
+        <select name="" id="">
+          <option value="2023IB01">2023IB01</option>
+        </select>
+      </div>
+      <div className="item-holder">
+        <label>Amount</label>
+        <input type="number" defaultValue={0} />
+      </div>
+      <div className="item-holder">
+        <label>Remark</label>
+        <input type="text" />
+      </div>
+      <div className="item-holder">
+        <input className="transfer-button" type="button" value={"Transfer"} />
+      </div>
+    </div>
+  );
+}
